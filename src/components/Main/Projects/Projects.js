58b9610eb0f05c903096010bc559dda3ec97df27
kimjin-012 <77659317+kimjin-012@users.noreply.github.com>
2021-05-21 23:04:19 -0700

    working with project page

--- src/components/Main/Projects/Projects.js
+++ src/components/Main/Projects/Projects.js
@@ -4,10 +4,34 @@ import './Projects.css'
 const Projects = () => {
     return (
         <>
-        <sectionn className="services section" id="services">
+        <section className="projects section" id="projects">
             <h2 className="section_title">Projects</h2>
-            <span className="section_subtitle"></span>
-        </sectionn>
+            <span className="section_subtitle">What I've made</span>
+
+            <div className="projects_container container grid">
+                {/* First Project : Share tracker */}
+                <div className="projects_content">
+                    <div className="projects_header">
+                        <i class="uil uil-brain projects_icon"></i>
+                        <div>
+                            <h1 className="projects_title">Languages</h1>
+                            <span className="projects_subtitle">Programming Languages</span>
+                        </div>
+                    </div>
+                    
+                    <div className="projects_list grid">
+                        <div className="projects_data container_flex">
+                            <div className="projects_title">
+                                <p className="projects_descripition">Hellooooo about this project</p>
+                            </div>
+                            <div className="projects_title">
+                                <img src="" alt="project image" className="projects_img" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
